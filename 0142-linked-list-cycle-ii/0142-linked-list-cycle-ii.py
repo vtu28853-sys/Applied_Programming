d05b8cf23037d4f9663b9fb09c72faa788e0b145
vtu28853-sys <vtu28853@veltech.edu.in>
2026-08-15 15:15:57 +0530

    Time: 51 ms (68.71%), Space: 22.3 MB (75.46%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,31 +1,30 @@
-# Definition for singly-linked list.
+# Definition for singly-linked list node.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
 class Solution:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def detectCycle(self, head: ListNode) -> ListNode:
         # Phase 1: Detect if there is a cycle
-        slow = fast = head
+        slow = head
+        fast = head
         
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+            slow = slow.next          # Move 1 step
+            fast = fast.next.next     # Move 2 steps
             
-            if slow == fast:
-                # Cycle detected, break to find the start
+            if slow == fast:          # Cycle detected
                 break
         else:
-            # If the loop completed without breaking, no cycle exists
-            return None
+            return None  # No cycle found (fast reached the end)
         
         # Phase 2: Find the start of the cycle
         # Reset slow to head, keep fast at meeting point
+        # Move both 1 step at a time until they meet
         slow = head
-        
         while slow != fast:
             slow = slow.next
             fast = fast.next
-            
-        return slow
+        
+        return slow  # This is the start of the cycle
